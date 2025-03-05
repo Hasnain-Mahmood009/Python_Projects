e56618f2f8e218abcf551990f179c41f9684298a
Hasnain-Mahmood009 <mahammadhasnain8080@gmail.com>
2025-03-05 11:15:15 -0800

    Added stack.py: Implemented a stack with push,pop and peek operations in Python

---
+++ Python_DSA_Files/Stack.py
@@ -0,0 +1,35 @@
+class stack:
+    def __init__(self):
+        self.item = []
+    def is_empty(self):
+        return len(self.item) == 0
+    def push(self,data):
+        self.item.append(data)
+    def pop(self):
+        if not self.is_empty():
+            return self.item.pop()
+        else:
+            raise IndexError("stack is empty")
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("stack is empty")
+        else:
+            return self.item[-1]
+    def size(self):
+        return len(self.item)
+s1 = stack()
+s1.push(10)
+s1.push(20)
+s1.push(30)
+s1.push(40)
+print("Top element is :", s1.peek())
+print("Removed element is :", s1.pop())
+print("Top element is :", s1.peek())
+print("Removed element is :", s1.pop())
+print("Top element is :", s1.peek())
+print("Removed element is :", s1.pop())
+print("Top element is :", s1.peek())
+
+
+
+
