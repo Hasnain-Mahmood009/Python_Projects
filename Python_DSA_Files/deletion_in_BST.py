0f38e6a3f64b4ea60042d7ad6cab1ad7aef0e4aa
Hasnain-Mahmood009 <mahammadhasnain8080@gmail.com>
2025-06-01 10:02:31 -0700

    Added min, max, deletion, and length features to existing DSA tree

---
+++ Python_DSA_Files/deletion_in_BST.py
@@ -0,0 +1,91 @@
+class Node:
+    def __init__(self,item=None,left=None,right=None):
+        self.item = item
+        self.left = left
+        self.right = right
+class BST:
+    def __init__(self):
+        self.root = None
+        self.item_count = 0
+    def insert(self,data):
+        self.root = self.rinsert(self.root,data)
+        self.item_count =+ 1
+    def rinsert(self,root,data):
+        if root is None:
+            return Node(data)
+        if data < root.item:
+            root.left = self.rinsert(root.left,data)
+        elif data > root.item:
+            root.right = self.rinsert(root.right,data)
+        return root
+    def search(self,data):
+        return self.rsearch(self.root,data)
+    def rsearch(self,root,data):
+        if root is None or root == data:
+            return root
+        if data < root.item:
+            return self.rsearch(root.left,data)
+        else:
+            return self.rsearch(root.right,data)
+    def inorder(self):
+        result = []
+        self.rinorder(self.root,result)
+        return result
+    def rinorder(self,root,result):
+        if root:
+            self.rinorder(root.left,result)
+            result.append(root.item)
+            self.rinorder(root.right,result)
+    def preorder(self):
+        result = []
+        self.rpreorder(self.root,result)
+        return result
+    def rpreorder(self,root,result):
+        if root:
+            result.append(root.item)
+            self.rpreorder(root.left,result)
+            self.rpreorder(root.right,result)
+    def postorder(self):
+        result = []
+        self.rpostorder(self.root,result)
+        return result
+    def rpostorder(self,root,result):
+        if root:
+            self.rpostorder(root.left,result)
+            self.rpostorder(root.right,result)
+            result.append(root.item)
+    def min_value(self):
+        result = []
+        self.rmin(self.root, result)
+        return min(result)        
+    def rmin(self,root,result):
+        if root:
+            root.left = self.rmin(root.left,result)
+            result.append(root.item)
+    def max_value(self):
+        result = []
+        self.rmax(self.root, result)
+        return max(result)        
+    def rmax(self,root,result):
+        if root:
+            root.right = self.rmax(root.right,result)
+            result.append(root.item)
+    
+    def delete(self,data):
+        self.root = self.rdelete(self.root,data)
+        self.item_count -= 1
+    def rdelete(self,root,data):
+        if root is None:
+            return root
+        elif data < root.item:
+            root.left = self.rdelete(root.left,data)
+        elif data > root.item:
+            root.right = self.rdelete(root.right, data)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            root.item = self.min_value(root.right)
+            self.rdelete(root.right,root.item)
+        return root
