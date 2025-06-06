fed0bab186c1b249e75187c4dbb41bd5d0bf14cf
Hasnain-Mahmood009 <mahammadhasnain8080@gmail.com>
2025-06-06 13:29:07 -0700

    Adding Modified_bubble_sort

--- Python_DSA_Files/modified_bubble_sort.py
+++ Python_DSA_Files/modified_bubble_sort.py
@@ -1,9 +1,14 @@
 def datalist(my_list):
+    flag = False
     n = len(my_list)
     for i in range(n):
+        flag = False
         for j in range(0,n-i-1):
             if my_list[j] > my_list[j+1]:
                 my_list[j],my_list[j+1] = my_list[j+1],my_list[j]
+                flag = True
+            if not flag:
+                break
     print(my_list)
 
 li = [2,4,2,1,3,5]
