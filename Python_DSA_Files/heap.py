f7380f2250bc019c62e86a530a37f5b739d4d9f5
Hasnain-Mahmood009 <mahammadhasnain8080@gmail.com>
2025-06-11 06:07:21 -0700

    Implementing Heap data structure

---
+++ Python_DSA_Files/heap.py
@@ -0,0 +1,52 @@
+class Heap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, value):
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+    def delete(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_up(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.heap[index] > self.heap[parent]:
+                self.heap[index], self.heap[parent] = self.heap[parent], self.heap[index]
+                index = parent
+            else:
+                break
+
+    def _heapify_down(self, index):
+        size = len(self.heap)
+        while 2 * index + 1 < size:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            largest = index
+
+            if left < size and self.heap[left] > self.heap[largest]:
+                largest = left
+            if right < size and self.heap[right] > self.heap[largest]:
+                largest = right
+
+            if largest != index:
+                self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+                index = largest
+            else:
+                break
+hp = Heap()
+hp.insert(2)
+hp.insert(1)
+hp.insert(4)
+hp.insert(5)
+hp.insert(3)
+hp.insert(6)
+print(hp.heap)
