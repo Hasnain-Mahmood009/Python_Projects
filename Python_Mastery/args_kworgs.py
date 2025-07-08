056ce0235e155624da63a531b5d3a2a934662685
Hasnain-Mahmood009 <mahammadhasnain8080@gmail.com>
2025-07-08 17:07:04 +0500

    **kwargs

--- Python_Mastery/args_kworgs.py
+++ Python_Mastery/args_kworgs.py
@@ -32,3 +32,45 @@ def sum_even(*args):
             li.append(num)
     print(sum(li))
 sum_even(1, 2, 3, 4, 5)
+
+
+#------------------------------------------------------------------------------------------------------------------------->
+#                                                      --Kwargs--                                                         #
+#------------------------------------------------------------------------------------------------------------------------->
+# kwargs is similar to args, but there is one main difference: args converts all values 
+# into a tuple, whereas kwargs converts them into a dictionary
+
+def fun(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key} : {value}")
+fun(name="Hasnain", age = "16")
+
+# Write a function that prints student details like name, class, age, etc.
+def stu_detial(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key} : {value}")
+stu_detial(name="Hasnain", class_ = "10th", roll_no = "4")
+
+# Write a function that counts how many keyword arguments were passed.
+def count_keyword(**kwargs):
+    print(f"Total keyword arguments : {len(kwargs)}")
+count_keyword(name = "Hasnain", age = "16" , gender = "Male")
+
+# Write a function that returns all keyword argument keys as a list.
+def keyword_To_list(**kwargs):
+    li = []
+    for key,value in kwargs.items():
+        li.append(key)
+    print(li)
+keyword_To_list(name = "Hasnain", age = "16" , gender = "Male")
+
+# Write a function that prints all positional arguments first, then all keyword arguments.
+def fun(*args,**kwargs):
+    li = []
+    for item in args:
+        li.append(item)
+    for key,value in kwargs.items():
+        x = f"{key} : {value}"
+        li.append(x)
+    print(li)
+fun(1,2,3, name = "Hasnain", age = "16")
