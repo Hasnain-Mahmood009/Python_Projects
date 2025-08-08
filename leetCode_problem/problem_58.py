931910e74700cecfc51995abd8176b4f66aaa11b
Hasnain-Mahmood009 <mahammadhasnain8080@gmail.com>
2025-08-08 15:45:32 +0500

    Soup Servings

---
+++ leetCode_problem/problem_58.py
@@ -0,0 +1,29 @@
+from functools import lru_cache
+
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n > 5000:
+            return 1.0
+        n = (n + 24) // 25
+
+        @lru_cache(None)
+        def dp(a, b):
+            if a <= 0 and b <= 0:
+                return 0.5
+            if a <= 0 and b > 0:
+                return 1.0
+            if b <= 0 and a > 0:
+                return 0.0
+            return 0.25 * (
+                dp(a - 4, b) +
+                dp(a - 3, b - 1) +
+                dp(a - 2, b - 2) +
+                dp(a - 1, b - 3)
+            )
+
+        return dp(n, n)
+
+# Example usage
+sol = Solution()
+print(sol.soupServings(50))
+print(sol.soupServings(100))
